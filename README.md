27882f780aeb56f8126ae34869bf5ea7f9fe23ab
Geoff Soord <Geoff-Soord@SoftwareImaging.local>
2018-02-07 15:56:42 +0000

    Initial commit

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Here's an integration of LightwaveRF with Home Assistant (HASS).
 It's very crude, but does work. It's been working for me for several years with the 1st generation of LightWave kit. I have not tested this with the newer (2nd gen) Lightwave products.
 
 ## Caveat
-This code is provided as a starting point, but is not yet a complete solution. It may not work out of the box for you.
-
 This code may show your devices in the Home Assistant UI, but it may not be able to control the lights for you. The code speaks directly to your Lightwave WiFi Link. Obviously, if you don't have a WiFi Link, then this isn't going to work for you.
 
-Secondly, and most importantly, your device running home assistant (in my case a raspberry pi) needs to be "authorised" to send messages to the WiFi Link. This script does not handle this yet. If there's demand, I'll try to get this pairing / authorisation process into the HASS UI, so it's easy for everyone.
+Secondly, and most importantly, your device running home assistant (in my case a raspberry pi) needs to be "authorised" to send messages to the WiFi Link. This script does not handle this yet but I plan on implementing this soon.
 
 In the meantime, if you need help with the pairing part, let me know.
 
@@ -18,21 +16,13 @@ If you feel like trying, open a terminal on the device running home assistant an
 echo -ne "100,\!F*p." | nc -u -w1 192.168.1.110 9760
 ```
 
-This should then show a message on your WiFi Link asking you to pair the device. Push the button on the WiFi Link to accept this. Once done, you should be able to control your lights via Home Asssistant.
+This should then show a message on your WiFi Link asking you to pair the device. You have 12 seconds to push the button on the WiFi Link to accept this. Once done, you should be able to control your lights via Home Asssistant.
 
 ## Installation
-### Edit the Lightwave.Py file
-You'll see at the top of the file there are a list of rooms and devices. If you want to, edit this to match your lightwaverf app. In the app you'll see a list of rooms. They are in order. The first room is R1. The second is R2 and so on. If you tap into a room on the lightwave app, you'll see a list of devices in that room. Again, these are in order. The first is D1, the second is D2 and so on. This allows you to determine the identifier for each device.
-
-For example if the first room in my list is 'Kitchen' (R1) and within this room I have 3 light devices(D1), ceiling(D2), wall and cabinet (D3). Then my devices would be R1D1, R1D2, R1D3 respectively.
-
-Remember you don't need to edit the file. Just note that all of your switches may not show and also that the naming may be wrong if you do not.
-
-
 ### Adding the Lightwave Component to Home Assistant
-The lightwave.py file needs to be placed in the installation directory of Home Assistant. For me this is something like
+The lightwave.py file needs to be placed in the installation directory of Home Assistant. For me this is
 ```
-/usr/local/share/lib/python3.4/homeassistant/component/lights/
+/custom_components/lights/
 ``` 
 There are instructions to follow on the instructions on the home-assistant website. If you need help, let me know.
 
@@ -42,7 +32,19 @@ Now that the component is installed you will need to add the setup to you config
 ```
 light:
   - platform: lightwave
+    devices:
+      R1D1:
+        name: Room one Device one
+      R1D2:
+        name: Room one Device two
+      R8D3:
+        name: Room eight Device three
    
 ```
+Each **device** requires an **id** and a **name**. The **id** takes the form **R#D#** where **R#** is the room number 
+and **D#** is the device number.
 
-I think you'll also need to add your devices here. I'll update this section when I'm next near my home-assistant.
+If **devices** is missing from your configuration file the component will generate 15 rooms with 8 lights to help you identify the id of each light.
+
+### Acknowledgement
+Thanks to Chirag Desai for paving the way to getting this component done.
--- lightwave.py
+++ lightwave.py
@@ -1,59 +1,106 @@
 """
 homeassistant.components.light.lightwave
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Demo platform that implements lights.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Implements LightwaveRF lights.
 
+
+My understanding of the LightWave Hub is that devices cannot be discovered so must be
+registered manually. This is done in the configuration file:
+
+switch:
+  - platform: lightwave
+    devices:
+      R1D2:
+        name: Room one Device two
+      R2D1:
+        name: Room two Device one
+
+Each device requires an id and a name. The id takes the form R#D# where R# is the room number 
+and D# is the device number.
+
+If devices are missing the default is to generate 15 rooms with 8 lights. From this you will
+be able to determine the room and device number for each light.
+
+TODO: 
+Add a registration button. Until then the following command needs to be sent to the LightwaveRF hub:
+    echo -ne "100,\!F*p." | nc -u -w1 LW_HUB_IP_ADDRESS 9760
+
+When this is sent you have 12 seconds to acknowledge the message on the hub.
+
+For more details on the api see: https://api.lightwaverf.com/
 """
-import random
+
 import socket
+import voluptuous as vol
 
+from homeassistant.const import CONF_DEVICES, CONF_NAME
 from homeassistant.components.light import (
-    Light, ATTR_BRIGHTNESS)
+    Light, ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, PLATFORM_SCHEMA)
+import homeassistant.helpers.config_validation as cv
 
+DEVICE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string
+})
 
-LIGHT_COLORS = [
-    [237, 224, 33],
-    [255, 63, 111],
-]
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_DEVICES, default={}): {cv.string: DEVICE_SCHEMA}
+})
 
-LIGHT_TEMPS = [240, 380]
-
-# Below are generic rooms, with sample lightwave device IDs e.g. R1D1.
-# At the moment we have to manually configure your LightWave Devices. If you need help getting the codes for your devices, let me know.
-# You could try my devices and see how they work. Basically R represents a room. D represents a device, as far as I can tell. Look at the readme file for more details.
 
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
-    """ Find and return lightwave lights """
-    add_devices_callback([
-        LRFLight("Guest Bedroom Light", False, 'R1D1'),
-        LRFLight("Office Ceiling Light", False, 'R2D1'),
-        LRFLight("Office Wall Light", False, 'R2D2'),
-        LRFLight("Living Room Wall Light", False, 'R3D1'),
-        LRFLight("Living Room Light", False, 'R3D2'),
-        LRFLight("Master Bedroom Light", False, 'R4D1')
-    ])
+    """ Find and return LightWave lights """
+    lights = []
+
+    for device_id, device_config in config.get(CONF_DEVICES, {}).items():
+        name = device_config[CONF_NAME]
+        lights.append(LRFLight(name, device_id))
+
+    if len(lights) == 0:
+        # Config is empty so generate a default set of switches
+        for room in range(1, 15):
+            for device in range(1, 8):
+                name = "Room " + str(room) + " Device " + str(device)
+                device_id = "R" + str(room) + "D" + str(device)
+                lights.append(LRFLight(name, device_id))
+
+    add_devices_callback(lights)
+
+
+def send_command(msg):
+    """ send message to LightwaveRF hub."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(msg.encode('UTF-8'), ('255.255.255.255', 9760))
+    sock.close
+
+
+def calculate_brightness(brightness):
+    """Scale brightness from 0..255 to 0..32"""
+    return round((brightness * 32) / 255)
 
 
 class LRFLight(Light):
-    """ Provides a demo switch. """
-    # pylint: disable=too-many-arguments
-    def __init__(self, name, state, deviceid, brightness=255):
+    """ Provides a LightWave light. """
+
+    def __init__(self, name, device_id):
         self._name = name
-        self._state = state
-        #self._rgb = rgb or random.choice(LIGHT_COLORS)
-        #self._ct = ct or random.choice(LIGHT_TEMPS)
-        self._brightness = brightness
-        self._deviceid = deviceid
-        #self._device_id = device_id
+        self._device_id = device_id
+        self._state = None
+        self._brightness = 255
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_BRIGHTNESS
 
     @property
     def should_poll(self):
-        """ No polling needed for a demo light. """
+        """ No polling needed for a LightWave light. """
         return False
 
     @property
     def name(self):
-        """ Returns the name of the device if any. """
+        """ Returns the name of the LightWave light. """
         return self._name
 
     @property
@@ -61,73 +108,33 @@ class LRFLight(Light):
         """ Brightness of this light between 0..255. """
         return self._brightness
 
-    #@property
-    #def rgb_color(self):
-    #    """ rgb color value. """
-    #    return self._rgb
-
-    #@property
-    #def color_temp(self):
-    #    """ CT color temperature. """
-    #    return self._ct
-
-    @property
-    def deviceid(self):
-        """ The Lightwave Device ID """
-        return self._deviceid
-
     @property
     def is_on(self):
-        """ True if device is on. """
+        """ True if the LightWave light is on. """
         return self._state
 
-
-    def send_command(self, msg):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        #command = "666, !R2D1F1|Turn On|Babas Lights"
-        #register = "666, !F*p"
-        sock.sendto(msg.encode('UTF-8'), ('255.255.255.255', 9760))
-        sock.close
-
-    def calculate_brightness(self, brightness):
-        # the scale is 0 to 255 so we need to normalise to 0 to 100 first.
-        old_range = 255 # 255 - 0 = 255
-        new_range = 100 # 100 - 9 = 100
-        new_value = (((brightness - 0) * new_range) / old_range)
-        brightness32 = round(new_value * 0.32)
-        return brightness32
-
-
     def turn_on(self, **kwargs):
-        """ Turn the device on. """
+        """ Turn the LightWave light on. """
         self._state = True
-
-        #if ATTR_RGB_COLOR in kwargs:
-        #    self._rgb = kwargs[ATTR_RGB_COLOR]
-
-        #if ATTR_COLOR_TEMP in kwargs:
-        #    self._ct = kwargs[ATTR_COLOR_TEMP]
 
         if ATTR_BRIGHTNESS in kwargs:
             self._brightness = kwargs[ATTR_BRIGHTNESS]
-            brightness_value = self.calculate_brightness(self._brightness)
-            msg = '666, !%sFdP%d|Lights %d|%s ' % (self._deviceid, brightness_value, brightness_value, self._name)
-            self.send_command(msg)
- 
-        print("LIGHTWAVE: Turning Lights On")
-        #F1 = Light on and F0 = light off. FdP[0..32] is brightness. 32 is full. We want that when turning the light on.
-        msg = '666, !%sFdP32|Turn On|%s ' % (self._deviceid, self._name)
-        self.send_command(msg)
-        print("LIGHTWAVE: finished")
+            brightness_value = calculate_brightness(self._brightness)
+            msg = '321, !%sFdP%d|Lights %d|%s ' % (
+                self._device_id, brightness_value, brightness_value, self._name)
+            send_command(msg)
+
+        # F1 = Light on and F0 = light off. FdP[0..32] is brightness. 32 is
+        # full. We want that when turning the light on.
+        msg = '321, !%sFdP32|Turn On|%s ' % (self._device_id, self._name)
+        send_command(msg)
 
         self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
-        """ Turn the device off. """ 
-        print("LIGHTWAVE: Turning Lights Off")
-        msg = "666, !%sF0|Turn Off|%s " % (self._deviceid, self._name)
-        self.send_command(msg)
-        print("LIGHTWAVE: finished")
+        """ Turn the LightWave light off. """
         self._state = False
+
+        msg = "321, !%sF0|Turn Off|%s " % (self._device_id, self._name)
+        send_command(msg)
         self.schedule_update_ha_state()
